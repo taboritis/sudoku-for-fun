8caf83ef04e42c35e5bfcc5d722b5fdda73b625c
Piotr Skrobol <taboritis@gmail.com>
2024-11-11 10:55:59 +0100

    WIP

--- src/Row.php
+++ src/Row.php
@@ -6,15 +6,46 @@ namespace Taboritis\Sudoku;
 
 class Row
 {
+    /**
+     * @var Cell[]
+     */
     private array $cells;
 
+    /**
+     * @param Cell[] $cells
+     */
     public function __construct(array $cells)
     {
+        $rows = array_unique(array_map(fn(Cell $cell) => $cell->getRow(), $cells));
+        $columns = array_unique(array_map(fn(Cell $cell) => $cell->getColumn(), $cells));
+
+        if (count($rows) > 1 || count($columns) !== 9) {
+            throw new \InvalidArgumentException('Cells are from different rows');
+        }
+
         $this->cells = $cells;
     }
 
+    /**
+     * @return Cell[]
+     */
     public function getCells(): array
     {
         return $this->cells;
+    }
+
+    public function getRowNumber(): int
+    {
+        return $this->cells[0]->getRow();
+    }
+
+    public function getCell(int $column): Cell
+    {
+        foreach ($this->cells as $cell) {
+            if ($cell->getColumn() === $column) {
+                return $cell;
+            }
+        }
+        throw new \InvalidArgumentException('Cell not found');
     }
 }
--- tests/RowTest.php
+++ tests/RowTest.php
@@ -30,4 +30,107 @@ class RowTest extends TestCase
 
         $this->assertCount(9, $row->getCells());
     }
+
+    #[Test]
+    public function it_has_a_number(): void
+    {
+        $cells = [
+            new Cell(5, 1),
+            new Cell(5, 2),
+            new Cell(5, 3),
+            new Cell(5, 4),
+            new Cell(5, 5),
+            new Cell(5, 6),
+            new Cell(5, 7),
+            new Cell(5, 8),
+            new Cell(5, 9)
+        ];
+
+        $column = new Row($cells);
+
+        $this->assertThat(5, $this->equalTo($column->getRowNumber()));
+    }
+
+    #[Test]
+    public function it_has_a_cells(): void
+    {
+        $cells = [
+            new Cell(5, 1),
+            new Cell(5, 2),
+            new Cell(5, 3),
+            new Cell(5, 4),
+            new Cell(5, 5),
+            new Cell(5, 6),
+            new Cell(5, 7),
+            new Cell(5, 8),
+            new Cell(5, 9)
+        ];
+
+        $column = new Row($cells);
+        $cell = $column->getCell(8);
+
+        $this->assertInstanceOf(Cell::class, $cell);
+        $this->assertEquals(8, $cell->getColumn());
+        $this->assertEquals(5, $cell->getRow());
+    }
+
+    #[Test]
+    public function a_row_has_9_cells(): void
+    {
+        $cells = [
+            new Cell(5, 1),
+            new Cell(5, 2),
+            new Cell(5, 3),
+            new Cell(5, 4),
+            new Cell(5, 5),
+            new Cell(5, 6),
+            new Cell(5, 7),
+            new Cell(5, 8),
+            new Cell(5, 9)
+        ];
+
+        $column = new Row($cells);
+
+        $this->assertCount(9, $column->getCells());
+    }
+
+    #[Test]
+    public function it_not_possible_to_create_row_with_cells_from_different_rows(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $cells = [
+            new Cell(5, 1),
+            new Cell(5, 2),
+            new Cell(5, 3),
+            new Cell(5, 4),
+            new Cell(5, 5),
+            new Cell(5, 6),
+            new Cell(5, 7),
+            new Cell(5, 8),
+            new Cell(6, 9)
+        ];
+
+        new Row($cells);
+    }
+
+    #[Test]
+    public function it_is_not_possible_to_create_row_with_duplicated_coordinates(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $cells = [
+            new Cell(5, 1),
+            new Cell(5, 1),
+            new Cell(5, 3),
+            new Cell(5, 4),
+            new Cell(5, 5),
+            new Cell(5, 6),
+            new Cell(5, 7),
+            new Cell(5, 8),
+            new Cell(5, 9)
+        ];
+
+        new Row($cells);
+    }
 }
